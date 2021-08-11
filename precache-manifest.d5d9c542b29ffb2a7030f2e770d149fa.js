5837170ac8d3fe30999a50d40e0a59d1721e4dd5
Teerapon Nampakdee <teerapon@kkumail.com>
2021-08-11 09:32:40 +0700

    deploy

---
+++ precache-manifest.d5d9c542b29ffb2a7030f2e770d149fa.js
@@ -0,0 +1,270 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce066ad51a91652a7579",
+    "url": "/blaq-lyte-wallet/css/app.655ce269.css"
+  },
+  {
+    "revision": "99fe7af20aaf66ff5996",
+    "url": "/blaq-lyte-wallet/css/chunk-045bb49f.d8df70e1.css"
+  },
+  {
+    "revision": "6b66d07132bbfd9de8f4",
+    "url": "/blaq-lyte-wallet/css/chunk-128aa79d.5818930b.css"
+  },
+  {
+    "revision": "8757640263dc9944f3c4",
+    "url": "/blaq-lyte-wallet/css/chunk-4542fd44.cac0f8bf.css"
+  },
+  {
+    "revision": "85d8706b963c743f6c8a",
+    "url": "/blaq-lyte-wallet/css/chunk-46f8955b.c916bb64.css"
+  },
+  {
+    "revision": "9951b08a2893134b8a25",
+    "url": "/blaq-lyte-wallet/css/chunk-51fa66dc.7629c375.css"
+  },
+  {
+    "revision": "277ed4f91f78c542fe67",
+    "url": "/blaq-lyte-wallet/css/chunk-5318e23d.b2539285.css"
+  },
+  {
+    "revision": "35b7da174a96f09a2c14",
+    "url": "/blaq-lyte-wallet/css/chunk-62257e90.81f78435.css"
+  },
+  {
+    "revision": "b61f882cf4eba6eae4d6",
+    "url": "/blaq-lyte-wallet/css/chunk-666bc473.3cb12c66.css"
+  },
+  {
+    "revision": "38cb21f4911477fba9ac",
+    "url": "/blaq-lyte-wallet/css/chunk-7b6940ae.dbbaf2ba.css"
+  },
+  {
+    "revision": "7f8e61cdf2b4ea1eaf35",
+    "url": "/blaq-lyte-wallet/css/chunk-7ba54752.38fb335c.css"
+  },
+  {
+    "revision": "0a7af3fa7f70561cf196",
+    "url": "/blaq-lyte-wallet/css/chunk-7ebddc7e.4740173c.css"
+  },
+  {
+    "revision": "3b3b7942acbd01d80bf7",
+    "url": "/blaq-lyte-wallet/css/chunk-88b95aac.c9b51c4c.css"
+  },
+  {
+    "revision": "b63979c40f39ea58e2b5",
+    "url": "/blaq-lyte-wallet/css/chunk-95b7d6f0.97f95f74.css"
+  },
+  {
+    "revision": "8c7b99c12b91107aa281",
+    "url": "/blaq-lyte-wallet/css/chunk-a890d2a6.93eea3b9.css"
+  },
+  {
+    "revision": "fa6f65afd26bb0e7fb98",
+    "url": "/blaq-lyte-wallet/css/chunk-d0a561b0.f46aa1d3.css"
+  },
+  {
+    "revision": "025b42e0090cfd6a983d",
+    "url": "/blaq-lyte-wallet/css/chunk-eec50a8a.f89150cb.css"
+  },
+  {
+    "revision": "966894949a53eda6f6b1",
+    "url": "/blaq-lyte-wallet/css/chunk-vendors.f876beec.css"
+  },
+  {
+    "revision": "d204df3d775c0c90d1773a97743a77b5",
+    "url": "/blaq-lyte-wallet/fonts/Kanit-Bold.d204df3d.ttf"
+  },
+  {
+    "revision": "3a5af91532287f228070af610add510c",
+    "url": "/blaq-lyte-wallet/fonts/Kanit-Light.3a5af915.ttf"
+  },
+  {
+    "revision": "b935eb6769e902b3b0086459a7c55a05",
+    "url": "/blaq-lyte-wallet/fonts/Kanit-Regular.b935eb67.ttf"
+  },
+  {
+    "revision": "802435446beb0e3178443e74b0d5a77f",
+    "url": "/blaq-lyte-wallet/fonts/Kanit-SemiBold.80243544.ttf"
+  },
+  {
+    "revision": "c748332120e330503843622ce96d6ce5",
+    "url": "/blaq-lyte-wallet/fonts/Kanit-Thin.c7483321.ttf"
+  },
+  {
+    "revision": "2a61b00fcc9b1afbf99be145777b1b72",
+    "url": "/blaq-lyte-wallet/fonts/MaterialIcons-Regular.2a61b00f.eot"
+  },
+  {
+    "revision": "51cf1d641ae503ece727e7d84b182524",
+    "url": "/blaq-lyte-wallet/fonts/MaterialIcons-Regular.51cf1d64.ttf"
+  },
+  {
+    "revision": "703cf8f274fbb265d49c6262825780e1",
+    "url": "/blaq-lyte-wallet/fonts/MaterialIcons-Regular.703cf8f2.woff2"
+  },
+  {
+    "revision": "84a37de85c17f186652a179b1145392f",
+    "url": "/blaq-lyte-wallet/fonts/MaterialIcons-Regular.84a37de8.woff"
+  },
+  {
+    "revision": "befa1f27b3fbec48e74d58e8f7d61bb8",
+    "url": "/blaq-lyte-wallet/fonts/Prompt-Bold.befa1f27.ttf"
+  },
+  {
+    "revision": "2aed5273600290713f7ed8d182a93592",
+    "url": "/blaq-lyte-wallet/fonts/Prompt-Light.2aed5273.ttf"
+  },
+  {
+    "revision": "16b4ce72cf30da14d83d5e3981d3113c",
+    "url": "/blaq-lyte-wallet/fonts/Prompt-Regular.16b4ce72.ttf"
+  },
+  {
+    "revision": "ba9b20ec2ff176605b2d69723af3a54f",
+    "url": "/blaq-lyte-wallet/fonts/Prompt-SemiBold.ba9b20ec.ttf"
+  },
+  {
+    "revision": "ddcfbf895b67206d003167df83c7020a",
+    "url": "/blaq-lyte-wallet/fonts/Prompt-Thin.ddcfbf89.ttf"
+  },
+  {
+    "revision": "027a885338265b4c26a79acc77fbb829",
+    "url": "/blaq-lyte-wallet/image/Image13@3x.png"
+  },
+  {
+    "revision": "1b3306cf7c3a7321de506ed7d63f15bc",
+    "url": "/blaq-lyte-wallet/image/buyicon.png"
+  },
+  {
+    "revision": "0c2aa197c13c31c18c03d84cf82926ba",
+    "url": "/blaq-lyte-wallet/image/buyiconactive.png"
+  },
+  {
+    "revision": "0e4f8cb4806d012a82598e866b93a66d",
+    "url": "/blaq-lyte-wallet/image/checked.png"
+  },
+  {
+    "revision": "800f55159a95e8ba43dd0e1f95677add",
+    "url": "/blaq-lyte-wallet/image/complain.png"
+  },
+  {
+    "revision": "358cb7fed48f47b71b1703376ef3dfd9",
+    "url": "/blaq-lyte-wallet/image/error.png"
+  },
+  {
+    "revision": "7557d01f74e9ec1480784754a83d5128",
+    "url": "/blaq-lyte-wallet/image/kplus.png"
+  },
+  {
+    "revision": "daaf344838497480993e43f30ba88712",
+    "url": "/blaq-lyte-wallet/image/logo.svg"
+  },
+  {
+    "revision": "8097ce7407b0d3a8a59e2f04c567a5bc",
+    "url": "/blaq-lyte-wallet/img/buyicon.8097ce74.svg"
+  },
+  {
+    "revision": "bb2c31e6501e6845848344f7d05d1222",
+    "url": "/blaq-lyte-wallet/img/content.bb2c31e6.svg"
+  },
+  {
+    "revision": "6dfac45c938c60d1fe1cbc7686dcfb22",
+    "url": "/blaq-lyte-wallet/img/default-user-icon.6dfac45c.svg"
+  },
+  {
+    "revision": "ff1676de3f5cf4b581854b49a6d0ec26",
+    "url": "/blaq-lyte-wallet/img/qr-code-button-icon.ff1676de.svg"
+  },
+  {
+    "revision": "da7d2d46db26764ae903e9295f7e5155",
+    "url": "/blaq-lyte-wallet/img/wallet-icon.da7d2d46.svg"
+  },
+  {
+    "revision": "04d09a7ce410e32cb545167a5ff6d057",
+    "url": "/blaq-lyte-wallet/index.html"
+  },
+  {
+    "revision": "ce066ad51a91652a7579",
+    "url": "/blaq-lyte-wallet/js/app.efce66bf.js"
+  },
+  {
+    "revision": "99fe7af20aaf66ff5996",
+    "url": "/blaq-lyte-wallet/js/chunk-045bb49f.b9e3497a.js"
+  },
+  {
+    "revision": "6b66d07132bbfd9de8f4",
+    "url": "/blaq-lyte-wallet/js/chunk-128aa79d.a7d65028.js"
+  },
+  {
+    "revision": "766cf7a0eefc12164e3b",
+    "url": "/blaq-lyte-wallet/js/chunk-2d0b6cf7.0fbcc088.js"
+  },
+  {
+    "revision": "8757640263dc9944f3c4",
+    "url": "/blaq-lyte-wallet/js/chunk-4542fd44.f1176868.js"
+  },
+  {
+    "revision": "85d8706b963c743f6c8a",
+    "url": "/blaq-lyte-wallet/js/chunk-46f8955b.80e95b8b.js"
+  },
+  {
+    "revision": "9951b08a2893134b8a25",
+    "url": "/blaq-lyte-wallet/js/chunk-51fa66dc.e52bfee2.js"
+  },
+  {
+    "revision": "277ed4f91f78c542fe67",
+    "url": "/blaq-lyte-wallet/js/chunk-5318e23d.42594e86.js"
+  },
+  {
+    "revision": "35b7da174a96f09a2c14",
+    "url": "/blaq-lyte-wallet/js/chunk-62257e90.a6adba6a.js"
+  },
+  {
+    "revision": "b61f882cf4eba6eae4d6",
+    "url": "/blaq-lyte-wallet/js/chunk-666bc473.0c1b5770.js"
+  },
+  {
+    "revision": "38cb21f4911477fba9ac",
+    "url": "/blaq-lyte-wallet/js/chunk-7b6940ae.039c0c8d.js"
+  },
+  {
+    "revision": "7f8e61cdf2b4ea1eaf35",
+    "url": "/blaq-lyte-wallet/js/chunk-7ba54752.f5ef31d0.js"
+  },
+  {
+    "revision": "0a7af3fa7f70561cf196",
+    "url": "/blaq-lyte-wallet/js/chunk-7ebddc7e.b813b34b.js"
+  },
+  {
+    "revision": "3b3b7942acbd01d80bf7",
+    "url": "/blaq-lyte-wallet/js/chunk-88b95aac.7a0216f7.js"
+  },
+  {
+    "revision": "b63979c40f39ea58e2b5",
+    "url": "/blaq-lyte-wallet/js/chunk-95b7d6f0.3562546e.js"
+  },
+  {
+    "revision": "8c7b99c12b91107aa281",
+    "url": "/blaq-lyte-wallet/js/chunk-a890d2a6.50f43516.js"
+  },
+  {
+    "revision": "fa6f65afd26bb0e7fb98",
+    "url": "/blaq-lyte-wallet/js/chunk-d0a561b0.2e3422cb.js"
+  },
+  {
+    "revision": "025b42e0090cfd6a983d",
+    "url": "/blaq-lyte-wallet/js/chunk-eec50a8a.d12e4b79.js"
+  },
+  {
+    "revision": "966894949a53eda6f6b1",
+    "url": "/blaq-lyte-wallet/js/chunk-vendors.55894fc3.js"
+  },
+  {
+    "revision": "458d7935d6e7973d4d9a5a62b9e6e5f8",
+    "url": "/blaq-lyte-wallet/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/blaq-lyte-wallet/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,21 @@
+importScripts("/blaq-lyte-wallet/precache-manifest.d5d9c542b29ffb2a7030f2e770d149fa.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+
+
+workbox.core.setCacheNameDetails({prefix: "mpv"});
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
